bca67b39e8f93d960ec191900f30a854e43fe4c4
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-02 13:24:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = {driver};
+  newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver;
+}
